929db4e870cdd9844c40375e2324953b0cc78c4c
esbrinachain <esbrinachain@gmail.com>
2024-05-13 15:52:05 +0000

    ok

--- 03_levantar_Peers.sh
+++ 03_levantar_Peers.sh
@@ -1,6 +1,6 @@
 # Acabar de levantar la red
 
-docker compose up -d peer1-org1 peer2-org1 peer1-org2 peer2-org2
+docker compose up -d 
 
 docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}'
 

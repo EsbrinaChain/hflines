7270c5938fb889313769bf4aa6393367ae93c028
esbrinachain <esbrinachain@gmail.com>
2024-05-17 08:52:58 +0000

    ok

--- 13_chaincode_invoke.sh
+++ 13_chaincode_invoke.sh
@@ -11,10 +11,4 @@ peer chaincode invoke \
    --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE_ORG1 \
    --peerAddresses localhost:9051 \
    --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE_ORG2 \
-   -c '{"Args":["registrarJamon", \
-                "1", \
-                "Iberico", \
-                "Bellota", \
-                "Dehesa de Extremadura", \
-                "Dehesa de Extremadura", \ 
-                "50"]}'
+   -c '{"Args":["registrarJamon", "1", "Iberico","Bellota","Dehesa de Extremadura","Dehesa de Extremadura", "50"]}'

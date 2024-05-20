c48715ef13561e02166def815d15d2432b01801a
esbrinachain <esbrinachain@gmail.com>
2024-05-20 18:05:52 +0000

    ok

---
+++ 13_chaincode_invoke-org1.sh
@@ -0,0 +1,16 @@
+# Muestra para la ejecución de funciones del chaincode.
+
+source ./term-org1
+
+peer chaincode invoke \
+   -o $ORDERER_ORG1_ADDRESS \
+   --ordererTLSHostnameOverride orderer1-org1 \
+   --tls $CORE_PEER_TLS_ENABLED \
+   --cafile $ORDERER_CA \
+   -C mychannel \
+   -n chaincode \
+   --peerAddresses $CORE_PEER_ADDRESS_ORG1 \
+   --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE_ORG1 \
+   --peerAddresses $CORE_PEER_ADDRESS_ORG2 \
+   --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE_ORG2 \
+   -c '{"Args":["registrarJamon", "1", "Iberico","Bellota","Dehesa de Extremadura","Dehesa de Extremadura", "50"]}'
--- 13_chaincode_invoke-org2.sh
+++ 13_chaincode_invoke-org2.sh
@@ -1,16 +1,16 @@
 # Muestra para la ejecución de funciones del chaincode.
 
-source ./term-org1
+source ./term-org2
 
 peer chaincode invoke \
-   -o localhost:7050 \
-   --ordererTLSHostnameOverride orderer1-org1 \
+   -o $ORDERER_ORG1_ADDRESS \
+   --ordererTLSHostnameOverride orderer1-org2 \
    --tls $CORE_PEER_TLS_ENABLED \
    --cafile $ORDERER_CA \
    -C mychannel \
    -n chaincode \
-   --peerAddresses localhost:7051 \
+   --peerAddresses $CORE_PEER_ADDRESS_ORG1 \
    --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE_ORG1 \
-   --peerAddresses localhost:9051 \
+   --peerAddresses $CORE_PEER_ADDRESS_ORG2 \
    --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE_ORG2 \
    -c '{"Args":["registrarJamon", "1", "Iberico","Bellota","Dehesa de Extremadura","Dehesa de Extremadura", "50"]}'

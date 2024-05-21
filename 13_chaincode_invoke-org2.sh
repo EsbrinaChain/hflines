333de8b286f4860bc0dd090ec63fcdfd8fbd550d
esbrinachain <esbrinachain@gmail.com>
2024-05-21 20:22:07 +0000

    ok

--- 13_chaincode_invoke-org2.sh
+++ 13_chaincode_invoke-org2.sh
@@ -3,7 +3,7 @@
 source ./term-org2
 
 peer chaincode invoke \
-   -o $ORDERER_ORG1_ADDRESS \
+   -o $ORDERER_ORG2_ADDRESS \
    --ordererTLSHostnameOverride orderer1-org2 \
    --tls $CORE_PEER_TLS_ENABLED \
    --cafile $ORDERER_CA \
@@ -14,3 +14,17 @@ peer chaincode invoke \
    --peerAddresses $CORE_PEER_ADDRESS_ORG2 \
    --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE_ORG2 \
    -c '{"Args":["registrarJamon", "1", "Iberico","Bellota","Dehesa de Extremadura","Dehesa de Extremadura", "50"]}'
+
+
+   peer chaincode invoke \
+   -o $ORDERER_ORG2_ADDRESS \
+   --ordererTLSHostnameOverride orderer1-org2 \
+   --tls $CORE_PEER_TLS_ENABLED \
+   --cafile $ORDERER_CA \
+   -C mychannel \
+   -n chaincode \
+   --peerAddresses $CORE_PEER_ADDRESS_ORG1 \
+   --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE_ORG1 \
+   --peerAddresses $CORE_PEER_ADDRESS_ORG2 \
+   --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE_ORG2 \
+   -c '{"Args":["imprimirJamon", "1"]}'

1afa298ad7a5eacd1f8e43ee8b732aba34c2159d
esbrinachain <esbrinachain@gmail.com>
2024-05-30 18:13:34 +0000

    ok

---
+++ chcodes/hflines/src/main/java/org/bsm/hflines/mensaje.java
@@ -0,0 +1,4 @@
+package org.bsm.hflines;
+
+public class mensaje {
+}
---
+++ chcodes/hflines/src/test/java/org/bsm/hflines/HflinesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bsm.hflines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HflinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

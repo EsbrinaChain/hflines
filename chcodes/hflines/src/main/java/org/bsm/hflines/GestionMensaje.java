a6a1d5b3154ac8f37e88fb78d149f578c4a7f981
esbrinachain <esbrinachain@gmail.com>
2024-05-31 17:13:28 +0000

    ok

--- chcodes/hflines/src/main/java/org/bsm/hflines/GestionMensaje.java
+++ chcodes/hflines/src/main/java/org/bsm/hflines/GestionMensaje.java
@@ -1,4 +1,19 @@
 package org.bsm.hflines;
 
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Default;
+import org.hyperledger.fabric.contract.annotation.Info;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.shim.ChaincodeException;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
+import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class GestionMensaje {
 }
--- chcodes/hflines/src/main/java/org/bsm/hflines/Mensaje.java
+++ chcodes/hflines/src/main/java/org/bsm/hflines/Mensaje.java
@@ -1,4 +1,47 @@
 package org.bsm.hflines;
 
-public class Mensaje {
+import com.google.gson.annotations.SerializedName;
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+
+import java.util.List;
+
+@DataType
+public final class Mensaje {
+
+    @Property()
+    @SerializedName("id") private final String id;
+
+    @Property()
+    @SerializedName("emisor") private final String emisor;
+
+    @Property()
+    @SerializedName("destinatarios") private final List<String> destinatarios;
+
+    @Property()
+    @SerializedName("subject") private final String subject;
+
+    @Property()
+    @SerializedName("texto") private final String texto;
+
+    public Mensaje(final String id,
+                   final String emisor,
+                   final List<String> destinatarios,
+                   final String subject,
+                   final String texto){
+        this.id=id;
+        this.emisor=emisor;
+        this.destinatarios=destinatarios;
+        this.subject=subject;
+        this.texto=texto;
+
+    }
+
+    public String getId() { return id; }
+    public String getEmisor() { return emisor; }
+    public String getSubject() { return subject; }
+    public List<String> getDestinatarios() { return destinatarios; }
+    public String getTexto() { return texto; }
+
+
 }

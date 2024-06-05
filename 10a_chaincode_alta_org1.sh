91997bb2e2797d6a0b1e611fcfdbea1b3407fafd
esbrinachain <esbrinachain@gmail.com>
2024-06-05 16:14:45 +0000

    ok

--- 10a_chaincode_alta_org1.sh
+++ 10a_chaincode_alta_org1.sh
@@ -1,10 +1,12 @@
 # Muestra del parametro $1 
-# $1=../chaincode/build/install/chaincode/
+# $1=./chcodes/hflines_ch1/build/install/hflines_ch1/
+
+ruta_install=./chcodes/hflines_ch1/build/install/hflines_ch1
 
 source ./term-org1
 
 peer lifecycle chaincode package hflines_chaincode.tar.gz \
-               --path $1 \
+               --path ruta_install \
                --lang java \
                --label chaincode
 
@@ -16,14 +18,3 @@ peer lifecycle chaincode queryinstalled \
                --peerAddresses $CORE_PEER_ADDRESS_ORG1 \
                --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE
 
-peer lifecycle chaincode approveformyorg \
-    -o $ORDERER_ORG1_ADDRESS \
-    --ordererTLSHostnameOverride orderer1-org1 \
-    --tls --cafile $ORDERER_CA \
-    --channelID mychannel \
-    --name chaincode \
-    --version 1.0 \
-    --package-id $1 \
-    --sequence 1
-    
-    # $1 : hflines:c2066d1ea51ebe1c70db570860f9e56a064d13283729e43aae2eec04b33212a0
---
+++ 10b_chaincode_alta_org1.sh
@@ -0,0 +1,11 @@
+peer lifecycle chaincode approveformyorg \
+    -o $ORDERER_ORG1_ADDRESS \
+    --ordererTLSHostnameOverride orderer1-org1 \
+    --tls --cafile $ORDERER_CA \
+    --channelID mychannel \
+    --name chaincode \
+    --version 1.0 \
+    --package-id $1 \
+    --sequence 1
+    
+    # $1 : hflines:c2066d1ea51ebe1c70db570860f9e56a064d13283729e43aae2eec04b33212a0

1af4a8bf3aab0e1aa67f0898b77127747772ba97
esbrinachain <esbrinachain@gmail.com>
2024-06-06 18:03:36 +0000

    ok

---
+++ chcodes/hf2/src/main/java/com/bsm/hf2/main/hf2Application.java
@@ -0,0 +1,13 @@
+package com.bsm.hf2.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaincodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaincodeApplication.class, args);
+	}
+
+}

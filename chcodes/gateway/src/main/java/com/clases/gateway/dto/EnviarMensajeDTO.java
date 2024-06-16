0939087f4bf3632e0528f82b5b1011ad2296c587
esbrinachain <esbrinachain@gmail.com>
2024-06-16 10:55:27 +0000

    ok

--- chcodes/gateway/src/main/java/com/clases/gateway/dto/EnviarMensajeDTO.java
+++ chcodes/gateway/src/main/java/com/clases/gateway/dto/EnviarMensajeDTO.java
@@ -6,21 +6,21 @@ import lombok.Data;
 @Data
 public class EnviarMensajeDTO {
 
-    @ApiModelProperty(value = "Mensaje ID", example = "1")
+    @ApiModelProperty(value = "Mensaje ID", example = "id numerico")
     String MensajeId;
 
-    @ApiModelProperty(value = "Emisor", example = "Miguel Delibes")
+    @ApiModelProperty(value = "Emisor", example = "nombre del emisor del mensaje")
     String Emisor;
 
-    @ApiModelProperty(value = "Asunto", example = "Entrega urgente")
+    @ApiModelProperty(value = "Asunto", example = "asunto del mensaje")
     String Asunto;
 
-    @ApiModelProperty(value = "Receptor", example = "Albert Einstein")
+    @ApiModelProperty(value = "Receptor", example = "nombre del receptor del mensaje")
     String receptor;
 
-    @ApiModelProperty(value = "EnCopia", example = "Santiago Pérez, Blanca Paz")
+    @ApiModelProperty(value = "EnCopia", example = "nombres de los terceros receptores del mensaje")
     String EnCopia;
 
-    @ApiModelProperty(value = "Texto", example = "Se convoca una reunión.")
+    @ApiModelProperty(value = "Texto", example = "texto del mensaje")
     String Texto;
 }
